46463f9597f043f0010d7b3b3fb685381c5d37f7
Louis Chatain <louischatain123@gmail.com>
2025-11-07 18:42:52 +0100

    used data type with a class model and query parameters

--- router/post_blog.py
+++ router/post_blog.py
@@ -1,10 +1,21 @@
+from typing import Optional
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-router = APIRouter(
-    prefix="/blog",
-    tags=["blog"]
-)
+router = APIRouter(prefix="/blog", tags=["blog"])
 
-@router.post("/news")
-def create_blog():
-    pass
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    published: Optional[bool]
+
+
+@router.post("/create_blog")
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        "id": id,
+        "data": blog,
+        "version": version
+    }
