f3fe6aaf12aded67de12080f797a5c652aeb19e4
Louis Chatain <louischatain123@gmail.com>
2025-11-11 21:32:08 +0100

    added id for the userdisplay

--- schemas.py
+++ schemas.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
 
-class UserBase(BaseModel):
+class UserBase(BaseModel):  # data that we will receive from the user
     username: str
     email: str
     password: str
 
-class UserDisplay(BaseModel): # sends back only username and email in the respounse body in the docs
-    username: str
-    email: str
+class UserDisplay(BaseModel): # data that we will receive from the user
+    username: str               # sends back only username and email in
+    email: str                  # the response body in the docs
+    id: int
     class Config():
         orm_mode = True
