7bfcd42b7bb261f8c333d81fa32c0c5ce5d1e3b0
Louis Chatain <louischatain123@gmail.com>
2025-11-13 23:29:38 +0100

    added exception handler for custom exception

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.responses import FileResponse, JSONResponse, PlainTextResponse
 from db.models import Base
 from db.database import engine
+from exceptions import StoryException
 from router import article, get_blog, post_blog, user
 
 
@@ -14,6 +15,19 @@ app.include_router(article.router)
 favicon_path = "favicon.ico"
 
 
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=status.HTTP_418_IM_A_TEAPOT,
+        content={"detail": exc.name}
+    )
+
+
+# @app.exception_handler(HTTPException)
+# def custom_handler(request: Request, exc: StoryException):
+#     return PlainTextResponse(str(exc), status_code=status.HTTP_400_BAD_REQUEST)
+
+
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse(favicon_path)
@@ -22,5 +36,6 @@ async def favicon():
 @app.get("/", summary="Says 'Hello world!'", tags=["hello"])
 def index():
     return {"message": "Hello World!"}
+
 
 Base.metadata.create_all(engine)
