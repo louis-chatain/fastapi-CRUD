7e747b594c91964568d4d048b44c9edf53449d87
Louis Chatain <louischatain123@gmail.com>
2025-11-17 13:06:35 +0100

    can now upload files and store them locally

--- router/file.py
+++ router/file.py
@@ -1,12 +1,18 @@
-from fastapi import APIRouter, File
+import shutil
+from fastapi import APIRouter, File, UploadFile
 
-router = APIRouter(
-    prefix="/file",
-    tags=["file"]
-)
+router = APIRouter(prefix="/file", tags=["file"])
 
-@router.post("")
-def get_file(file: bytes = File(Ellipsis)):
-    content = file.decode("utf-8")
-    lines = content.split("\n")
-    return {"lines": lines}
+
+@router.post("/uploadFile")
+def upload_file(
+    upload_file: UploadFile = File(Ellipsis),
+):  # if a new uploaded file has the same name as a saved one, it will replaced it, not good.
+    file_location = f"files/{upload_file.filename}"
+    with open(file_location, "w+b") as buffer:
+        shutil.copyfileobj(upload_file.file, buffer)
+    return {
+        "filename": upload_file.filename,
+        "type": upload_file.content_type,
+        "info": f"file '{upload_file.filename}' saved at '{file_location}'",
+    }
