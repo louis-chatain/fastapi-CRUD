dc1ef7df9a1020ec510a3dbcb735718d338b5b99
Louis Chatain <louischatain123@gmail.com>
2025-11-23 15:37:16 +0100

    added backgrounds tasks

--- templates/templates.py
+++ templates/templates.py
@@ -1,6 +1,5 @@
-
-from turtle import title
-from fastapi import APIRouter, Request
+from log.custom_log import log
+from fastapi import APIRouter, BackgroundTasks, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -11,15 +10,21 @@ router = APIRouter(prefix="/templates", tags=["templates"])
 
 templates = Jinja2Templates(directory="templates")
 
-@router.post("/poducts/{id}", response_class=HTMLResponse)
-def get_product(id: int, product: ProductBase, request: Request):
+
+@router.post("/products/{id}", response_class=HTMLResponse)
+def get_product(id: int, product: ProductBase, request: Request, bt: BackgroundTasks):
+    bt.add_task(log_template_call, f"template read for product with id {id}")
     return templates.TemplateResponse(
         "products.html",
         {
             "request": request,
-            "id" : id,
+            "id": id,
             "title": product.title,
             "description": product.description,
-            "price": product.price
-        }
+            "price": product.price,
+        },
     )
+
+
+def log_template_call(message: str):
+    log("MyApi", message)
